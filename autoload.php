b4afe6b41a9e5409bb36b4d35ddccfb6a2770fdf
Lucas Gabriel <pintolucas996@gmail.com>
2023-02-23 16:11:44 -0300

    rotas e autoload

--- autoload.php
+++ autoload.php
@@ -1,2 +1,25 @@
 <?php
 // carregar as classe automaticamente
+
+spl_autoload_register(function ($nome_da_classe)
+{
+    $nome_da_classe = str_replace('\\', '/', $nome_da_classe)
+
+    $arquivo = BASEDIR . '/' . $nome_da_classe . '.php';
+
+    if(file_exists($arquivo))
+    {
+        include $arquivo;
+
+    }else
+        exit('Arquivo não encontrado. Arquivo: ' . $arquivo . " <br> ");
+
+});
+
+
+
+
+
+
+
+
